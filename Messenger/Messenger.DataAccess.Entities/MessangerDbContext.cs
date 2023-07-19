fcc16a8ffa8740e2884dba627820ec78b62f0591
Re9Wine <asckortan@gmail.com>
2023-07-19 22:44:21 +0300

    Update MessangerDbContext.cs

--- Messenger/Messenger.DataAccess.Entities/MessangerDbContext.cs
+++ Messenger/Messenger.DataAccess.Entities/MessangerDbContext.cs
@@ -6,36 +6,33 @@ namespace Messenger.DataAccess.Entities
 {
     public class MessangerDbContext : DbContext
     {
-        public class MessengerDbContext : DbContext
+        public MessangerDbContext()
         {
-            public MessengerDbContext()
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        public MessangerDbContext(DbContextOptions<MessangerDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserContact> UserContacts { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<UserInChat> UsersInChat { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
             {
-                Database.EnsureDeleted();
-                Database.EnsureCreated();
+                optionsBuilder.UseSqlServer("Server=localhost;Database=Messenger;Trusted_Connection=True;");
             }
+        }
 
-            public MessengerDbContext(DbContextOptions<MessengerDbContext> options)
-                : base(options) { }
-
-            public DbSet<User> Users { get; set; }
-            public DbSet<UserContact> UserContacts { get; set; }
-            public DbSet<Message> Messages { get; set; }
-            public DbSet<Chat> Chats { get; set; }
-            public DbSet<UserInChat> UsersInChat { get; set; }
-
-            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            {
-                if (!optionsBuilder.IsConfigured)
-                {
-                    optionsBuilder.UseSqlServer("Server=localhost;Database=Messenger;Trusted_Connection=True;");
-                }
-            }
-
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
-            {
-                modelBuilder.ChatModelCreating();
-                modelBuilder.UserModelCreating();
-            }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ChatModelCreating();
+            modelBuilder.UserModelCreating();
         }
     }
 }
